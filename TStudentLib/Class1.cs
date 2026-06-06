5efa1e1ebdf92a650f29a7b9077c6f9a398b12f0
Alexis Nata <alexis2000nata@gmail.com>
2026-06-05 23:40:42 -0500

    Implementar el metodo calcularMedia

--- TStudentLib/Class1.cs
+++ TStudentLib/Class1.cs
@@ -1,6 +1,17 @@
 ﻿namespace TStudentLib;
 
-public class Class1
+public class TStudentCalculator
 {
+    // MÉTODO 1: Calcular Media
+    public double CalcularMedia(double[] muestra)
+    {
+        if (muestra == null || muestra.Length == 0)
+            throw new ArgumentException("La muestra no puede ser nula o vacía.");
 
+        double suma = 0;
+        foreach (double valor in muestra)
+            suma += valor;
+
+        return suma / muestra.Length;
+    }
 }
