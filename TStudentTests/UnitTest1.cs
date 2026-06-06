f4d1691ebafaf94c9f0f4bd0fab9d45b02d714e3
Alexis Nata <alexis2000nata@gmail.com>
2026-06-05 23:27:36 -0500

    TDD red para el calculo de la media

--- TStudentTests/UnitTest1.cs
+++ TStudentTests/UnitTest1.cs
@@ -1,10 +1,25 @@
 ﻿namespace TStudentTests;
 
-public class UnitTest1
+public class TStudentCalculatorTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly TStudentLib.TStudentCalculator _calc;
 
+    public TStudentCalculatorTests()
+    {
+        _calc = new TStudentLib.TStudentCalculator();
+    }
+
+    // PRUEBA 1: Media
+    [Fact]
+    public void CalcularMedia_ConDatosValidos_RetornaMediaCorrecta()
+    {
+        double[] muestra = { 2, 4, 6, 8, 10 };
+        double resultadoEsperado = 6.0;
+
+        double resultado = _calc.CalcularMedia(muestra);
+
+        Assert.Equal(resultadoEsperado, resultado);
     }
 }
+
+
